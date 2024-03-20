542e4299a63dbfb44338a220a376ff3001897ddb
ViceLlobre <vicente.llobregat94@gmail.com>
2024-03-20 16:35:16 -0400

    Developed login controller applying TDD
    + Added LoginUserController class
    + Added LoginUserControllerTest class
    + Added TestConfigurationFactory class

---
+++ CoWorkingApp.Tests/TestConfigurationFactory.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace CoWorkingApp.Tests
+{
+    /// <summary>
+    /// Clase para la creación de instancias de IConfiguration utilizadas en las pruebas.
+    /// </summary>
+    public class TestConfigurationFactory
+    {
+        /// <summary>
+        /// Crea y devuelve una instancia de IConfiguration configurada con los valores proporcionados en un diccionario.
+        /// </summary>
+        /// <param name="configValues">Diccionario de valores de configuración.</param>
+        /// <returns>Instancia de IConfiguration configurada con los valores proporcionados.</returns>
+        public static IConfiguration CreateConfiguration(Dictionary<string, string> configValues)
+        {
+            // Configura la configuración en memoria
+            IConfiguration configuration = new ConfigurationBuilder()
+                .AddInMemoryCollection(configValues)
+                .Build();
+
+            // Devuelve la configuración
+            return configuration;
+        }
+    }
+}
