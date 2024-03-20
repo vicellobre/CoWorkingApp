1dbf08a9ad959467c5937e5fe0fc5b2988602f0d
ViceLlobre <vicente.llobregat94@gmail.com>
2024-03-20 16:03:52 -0400

    Defined IReservationService interface and developed SeatRequest and SeatResponse classes applying TDD
    + Added IReservationService class
    + Added ReservationRequest class
    + Added ReservationResponse class
    + Added ReservationRequestTests class
    + Added ReservationResponseTest class

---
+++ CoWorkingApp.Core/Domain/DTOs/ReservationRequest.cs
@@ -0,0 +1,25 @@
+﻿using CoWorkingApp.Core.Application.Contracts.Requests;
+
+namespace CoWorkingApp.Core.Domain.DTOs
+{
+    /// <summary>
+    /// Representa la solicitud de una reserva en el sistema.
+    /// </summary>
+    public class ReservationRequest : IRequest
+    {
+        /// <summary>
+        /// Obtiene o establece la fecha de la reserva.
+        /// </summary>
+        public DateTime Date { get; set; }
+
+        /// <summary>
+        /// Obtiene o establece el identificador único del usuario asociado a la reserva.
+        /// </summary>
+        public Guid UserId { get; set; }
+
+        /// <summary>
+        /// Obtiene o establece el identificador único del asiento asociado a la reserva.
+        /// </summary>
+        public Guid SeatId { get; set; }
+    }
+}
