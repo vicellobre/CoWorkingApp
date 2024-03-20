3582b5333c87232e7c3b5b7b8f182fd2f8a72c6a
ViceLlobre <vicente.llobregat94@gmail.com>
2024-03-20 17:03:52 -0400

    Developed reservation controller applying TDD
    + Added ReservationController class
    + Added ReservationControllerTest class
    + Added InvalidSuccessResponsesClassData class

---
+++ CoWorkingApp.Tests/ClassData/InvalidSuccessResponsesClassData.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+
+namespace CoWorkingApp.Tests.ClassData
+{
+    public class InvalidSuccessResponsesClassData : IEnumerable<object[]>
+    {
+        private readonly List<object[]> _data = new List<object[]>()
+        {
+            new object[] { false, true, true },
+            new object[] { true, false, true },
+            new object[] { true, true, false },
+            new object[] { false, false, true },
+            new object[] { false, true, false},
+            new object[] { true, false, false},
+            new object[] { false, false, false },
+        };
+
+        public IEnumerator<object[]> GetEnumerator()
+        {
+            return _data.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
