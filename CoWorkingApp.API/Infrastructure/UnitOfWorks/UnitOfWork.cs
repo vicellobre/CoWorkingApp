44fa2416568c4c83b4e1da2805c4590338bf58a4
ViceLlobre <vicente.llobregat94@gmail.com>
2024-03-20 14:06:28 -0400

    Developed IUnitOfWork implementation applying TDD
    + Added UnitOfWork class
    + Added UnitOfWorkTest class for unit testing
    + Integrated Moq package

---
+++ CoWorkingApp.API/Infrastructure/UnitOfWorks/UnitOfWork.cs
@@ -0,0 +1,41 @@
+﻿using CoWorkingApp.API.Infrastructure.Context;
+
+namespace CoWorkingApp.API.Infrastructure.UnitOfWorks
+{
+    /// <summary>
+    /// Implementación de la interfaz IUnitOfWork para gestionar transacciones y operaciones de base de datos.
+    /// </summary>
+    public class UnitOfWork : IUnitOfWork
+    {
+        /// <summary>
+        /// Obtiene el contexto de la base de datos utilizado para acceder y manipular los datos.
+        /// </summary>
+        public CoWorkingContext Context { get; }
+
+        /// <summary>
+        /// Constructor que inicializa una nueva instancia de UnitOfWork con el contexto de la base de datos proporcionado.
+        /// </summary>
+        /// <param name="dbContext">Contexto de la base de datos (CoWorkingContext).</param>
+        public UnitOfWork(CoWorkingContext dbContext)
+        {
+            Context = dbContext ?? throw new ArgumentNullException(nameof(dbContext));
+        }
+
+        /// <summary>
+        /// Confirma los cambios realizados en el contexto de la base de datos.
+        /// </summary>
+        /// <returns>Una tarea asincrónica que representa la operación de confirmación.</returns>
+        public async Task Commit()
+        {
+            await Context.SaveChangesAsync();
+        }
+
+        /// <summary>
+        /// Libera los recursos no administrados utilizados por la instancia de UnitOfWork.
+        /// </summary>
+        public void Dispose()
+        {
+            Context.Dispose();
+        }
+    }
+}
---
+++ CoWorkingApp.Tests/Infrastructure/UnitOfWorks/UnitOfWorkTest.cs
@@ -0,0 +1,79 @@
+﻿using CoWorkingApp.API.Infrastructure.Context;
+using CoWorkingApp.API.Infrastructure.UnitOfWorks;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+
+namespace CoWorkingApp.Tests.Infrastructure.UnitOfWorks
+{
+    /// <summary>
+    /// Clase de pruebas para el controlador UnitOfWork.
+    /// </summary>
+    public class UnitOfWorkTest
+    {
+        /// <summary>
+        /// Verifica que el constructor de UnitOfWork cree una instancia válida cuando el contexto no es nulo.
+        /// </summary>
+        [Fact]
+        public void UnitOfWorkConstructor_Returns_ValidInstance_When_ContextIsNotNull()
+        {
+            // ARRANGE
+            var context = new Mock<CoWorkingContext>(new DbContextOptions<CoWorkingContext>());
+
+            // ACT
+            var result = new UnitOfWork(context.Object);
+
+            // ASSERT
+            Assert.NotNull(result);
+        }
+
+        /// <summary>
+        /// Verifica que el constructor de UnitOfWork lance una excepción cuando el contexto es nulo.
+        /// </summary>
+        [Fact]
+        public void UnitOfWorkConstructor_Throws_ArgumentNullException_When_ContextIsNull()
+        {
+            // ARRANGE
+            CoWorkingContext context = null;
+
+            // ACT
+            var result = () => new UnitOfWork(context);
+
+            // ASSERT
+            Assert.Throws<ArgumentNullException>(result);
+        }
+
+        /// <summary>
+        /// Verifica que el método CommitAsync de UnitOfWork llame a SaveChangesAsync exactamente una vez en el contexto del contexto de base de datos.
+        /// </summary>
+        [Fact]
+        public async Task CommitAsync_SavesChangesOnce_When_Called()
+        {
+            // ARRANGE
+            var dbContextMock = new Mock<CoWorkingContext>(new DbContextOptions<CoWorkingContext>());
+            var unitOfWork = new UnitOfWork(dbContextMock.Object);
+
+            // ACT
+            await unitOfWork.Commit();
+
+            // ASSERT
+            dbContextMock.Verify(db => db.SaveChangesAsync(It.IsAny<CancellationToken>()), Times.Once);
+        }
+
+        /// <summary>
+        /// Verifica que el método Dispose de UnitOfWork desecha el contexto de base de datos.
+        /// </summary>
+        [Fact]
+        public void Dispose_DisposesDatabaseContext_When_Called()
+        {
+            // ARRANGE
+            var dbContextMock = new Mock<CoWorkingContext>(new DbContextOptions<CoWorkingContext>());
+            var unitOfWork = new UnitOfWork(dbContextMock.Object);
+
+            // ACT
+            unitOfWork.Dispose();
+
+            // ASSERT
+            dbContextMock.Verify(db => db.Dispose(), Times.Once);
+        }
+    }
+}
