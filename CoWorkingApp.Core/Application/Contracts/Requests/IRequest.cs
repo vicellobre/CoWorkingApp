6b7a2a8a567873c0fc922444805aca309c2a8464
ViceLlobre <vicente.llobregat94@gmail.com>
2024-03-20 15:06:38 -0400

    Defined ResponseMessage class and IService/IRequest interfaces
    + Added RepositoryGeneric class
    + Added ResponseMessage class
    + Added IRequest.cs file
    + Added IRequestHandler.cs file

---
+++ CoWorkingApp.Core/Application/Contracts/Requests/IRequest.cs
@@ -0,0 +1,13 @@
+﻿namespace CoWorkingApp.Core.Application.Contracts.Requests
+{
+    /// <summary>
+    /// Interfaz marcadora para representar una solicitud sin respuesta.
+    /// </summary>
+    public interface IRequest { }
+
+    /// <summary>
+    /// Interfaz marcadora para representar una solicitud con respuesta.
+    /// </summary>
+    /// <typeparam name="TResponse">Tipo de respuesta</typeparam>
+    public interface IRequest<out TResponse> { }
+}
