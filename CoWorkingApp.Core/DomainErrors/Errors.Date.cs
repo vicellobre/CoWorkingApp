769dbb6a13d61cc7704ce3a9d807ad85479380c4
ViceLlobre <vicente.llobregat94@gmail.com>
2024-12-01 22:06:37 -0400

    Added more Errors

---
+++ CoWorkingApp.Core/DomainErrors/Errors.Date.cs
@@ -0,0 +1,17 @@
+﻿using CoWorkingApp.Core.Shared;
+
+namespace CoWorkingApp.Core.DomainErrors;
+
+/// <summary>
+/// Contiene los errores relacionados con Date.
+/// </summary>
+public static partial class Errors
+{
+    public static class Date
+    {
+        /// <summary>
+        /// La fecha es inválida.
+        /// </summary>
+        public static readonly Error Invalid = Error.Create("Date.Invalid", "The date is invalid.");
+    }
+}
---
+++ CoWorkingApp.Core/DomainErrors/Errors.SeatRow.cs
@@ -0,0 +1,17 @@
+﻿using CoWorkingApp.Core.Shared;
+
+namespace CoWorkingApp.Core.DomainErrors;
+
+/// <summary>
+/// Define errores predefinidos comunes utilizados en toda la aplicación.
+/// </summary>
+public static partial class Errors
+{
+    public static class SeatRow
+    {
+        /// <summary>
+        /// La fila del asiento no puede ser nula o vacía.
+        /// </summary>
+        public static readonly Error IsNullOrEmpty = Error.Create("SeatRow.IsNullOrEmpty", "Seat row cannot be null or empty.");
+    }
+}
