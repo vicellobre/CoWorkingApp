bdf462e076911efc9777c00f069565f71b1a0e36
ViceLlobre <vicente.llobregat94@gmail.com>
2024-12-03 11:00:58 -0400

    Added Errors for SeatName

---
+++ CoWorkingApp.Core/DomainErrors/Errors.SeatName.cs
@@ -0,0 +1,20 @@
+﻿using CoWorkingApp.Core.Shared;
+
+namespace CoWorkingApp.Core.DomainErrors;
+
+/// <summary>
+/// Define errores predefinidos comunes utilizados en toda la aplicación.
+/// </summary>
+public static partial class Errors
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public static class SeatName
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public static readonly Error InvalidFormat = Error.Create("InvalidFormat", "The value must be in the format 'Row-Number'.");
+    }
+}
