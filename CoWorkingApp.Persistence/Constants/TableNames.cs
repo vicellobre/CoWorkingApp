19a7eade3abaa2c547dc8674cae2f5715f47cdb4
ViceLlobre <vicente.llobregat94@gmail.com>
2024-12-03 11:02:45 -0400

    Added Contants

---
+++ CoWorkingApp.Persistence/Constants/TableNames.cs
@@ -0,0 +1,22 @@
+﻿namespace CoWorkingApp.Persistence.Constants;
+
+/// <summary>
+/// Proporciona los nombres de las tablas utilizadas en la persistencia de datos.
+/// </summary>
+internal static class TableNames
+{
+    /// <summary>
+    /// El nombre de la tabla de usuarios.
+    /// </summary>
+    internal const string Users = nameof(Users);
+
+    /// <summary>
+    /// El nombre de la tabla de asientos.
+    /// </summary>
+    internal const string Seats = nameof(Seats);
+
+    /// <summary>
+    /// El nombre de la tabla de reservas.
+    /// </summary>
+    internal const string Reservations = nameof(Reservations);
+}
