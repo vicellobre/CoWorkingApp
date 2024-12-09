0b440bc38e2b3b20bba90f05f322bda507f2dfe2
ViceLlobre <vicente.llobregat94@gmail.com>
2024-12-09 12:05:08 -0400

    Added reservation validators

---
+++ CoWorkingApp.Application/Reservations/Commands/UpdateReservation/UpdateReservationCommandValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+
+namespace CoWorkingApp.Application.Reservations.Commands.UpdateReservation;
+
+/// <summary>
+/// Validador para el comando <see cref="UpdateReservationCommand"/>.
+/// </summary>
+internal class UpdateReservationCommandValidator : AbstractValidator<UpdateReservationCommand>
+{
+    /// <summary>
+    /// Inicializa una nueva instancia de la clase <see cref="UpdateReservationCommandValidator"/>.
+    /// </summary>
+    public UpdateReservationCommandValidator()
+    {
+        RuleFor(x => x.Date)
+            .NotNull()
+            .GreaterThanOrEqualTo(DateTime.Now.Date);
+    }
+}
---
+++ CoWorkingApp.Application/Reservations/Queries/GetReservationsBySeatName/GetReservationsBySeatNameQueryValidator.cs
@@ -0,0 +1,23 @@
+﻿using CoWorkingApp.Core.ValueObjects.Composite;
+using FluentValidation;
+
+namespace CoWorkingApp.Application.Reservations.Queries.GetReservationsBySeatName;
+
+/// <summary>
+/// Validador para la consulta <see cref="GetReservationsBySeatNameQuery"/>.
+/// </summary>
+internal class GetReservationsBySeatNameQueryValidator : AbstractValidator<GetReservationsBySeatNameQuery>
+{
+    /// <summary>
+    /// Inicializa una nueva instancia de la clase <see cref="GetReservationsBySeatNameQueryValidator"/>.
+    /// </summary>
+    public GetReservationsBySeatNameQueryValidator()
+    {
+        RuleFor(x => x.SeatName)
+            .NotNull()
+            .NotEmpty()
+            .MinimumLength(SeatName.MinLength)
+            .MaximumLength(SeatName.MaxLength)
+            .Matches(SeatName.Pattern);
+    }
+}
