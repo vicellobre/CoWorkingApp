8d5183b053d824846bf1993033361dbf997bdf53
ViceLlobre <vicente.llobregat94@gmail.com>
2024-12-16 19:27:42 -0400

    Refactor Program class

--- CoWorkingApp.API/Program.cs
+++ CoWorkingApp.API/Program.cs
@@ -1,14 +1,6 @@
 using CoWorkingApp.API.Extensions.HostBuilder;
-using System.Diagnostics.CodeAnalysis;
 
-[ExcludeFromCodeCoverage]
-internal class Program
-{
-    private static void Main(string[] args)
-    {
-        Host.CreateDefaultBuilder(args)
+Host.CreateDefaultBuilder(args)
             .ConfigureHost()
             .Build()
             .Run();
-    }
-}
